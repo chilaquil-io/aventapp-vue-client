0f9f8d8fa660d6ea27031a531b8e3226b5b419db
Shamed Calderon <arzu.calderon@gmail.com>
2019-02-11 01:42:42 -0500

    Setup project

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,46 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div id="loginForm" class="card has-text-centered">
+      <header class="card-header" style="text-align: center;">
+        <p class="card-header-title">
+          <strong>Inicia sesión en Aventapp</strong>
+        </p>
+      </header>
+      <div class="card-content">
+        <div class="content">
+          <div class="field">
+            <div class="g-signin2" data-onsuccess="onSignIn"></div>
+          </div>
+        </div>
+      </div>
     </div>
-    <router-view/>
+    <!-- <div class="container">
+      
+    </div>-->
   </div>
 </template>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Comfortaa", cursive;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
+  background-color: #e1eff6;
+  height: 100vh;
 }
 
-#nav a.router-link-exact-active {
-  color: #42b983;
+#loginForm {
+  max-width: 500px;
+  border-radius: 7px;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  width: 50%;
+  height: 30%;
+  margin: auto;
 }
 </style>
